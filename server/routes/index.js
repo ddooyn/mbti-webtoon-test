70d4be4b17731eaaf9d06b511dbe8d9bd5bc31a3
Sooyoung Yoon <ayoooo240@gmail.com>
2022-07-12 14:41:43 +0900

    🔧 ESLint 설정 수정

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const app = express();
 const port = process.env.PORT || 5000;
